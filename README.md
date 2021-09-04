f2cfb0f5694fc819bfa1b634fc94c34b5bd2e547
kalyani joshi <kalyani121@gmail.com>
2021-09-04 17:45:38 -0400

    updated Readme with details

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# note-taker
+# Note-Taker
+
+## Description
+
+A business application to take and maintain notes. 
+
+## Technology Stack
+
+HTML,CSS,JavaScript,Node.js,Express.js
+
+### Installation
+
+1. Start localhost server/ server.js from terminal for the application.
+
+2. Open index.html in browser to get start with notes.
--- server.js
+++ server.js
@@ -22,12 +22,16 @@ app.get("/notes", (req, res) =>
 );
 
 //api for notes
+
+//retrieve all notes data
 app.get("/api/notes", (req, res) => {
   res.status(200).send(noteData);
 });
 
+//retrieve note by ID
 app.get("/api/notes/:id", (req, res) => {
   const noteId = parseInt(req.params.id, 10);
+  //validation to handle search by note id and pass status/ response based on match
   if (noteId) {
     const foundNote = noteData.find((n) => n.id === noteId);
     if (foundNote) {
@@ -40,10 +44,12 @@ app.get("/api/notes/:id", (req, res) => {
   }
 });
 
+//Save note data by Post method
 app.post("/api/notes", (req, res) => {
   try {
     console.log(req.body);
     let note = req.body;
+    //validation for empty request
     if (Object.keys(note).length) {
       let idArr = noteData.map((n) => n.id);
 
@@ -64,44 +70,10 @@ app.post("/api/notes", (req, res) => {
   }
 });
 
-app.put("/api/notes/:id", (req, res) => {
-  try {
-    console.log(req.body);
-
-    let note = req.body;
-    if (Object.keys(note).length) {
-      const noteId = parseInt(req.params.id, 10);
-      if (noteId) {
-        let noteIdx = noteData.findIndex((n) => n.id === noteId);
-        console.log(noteIdx);
-        if (noteIdx === -1) {
-          res.status(404).send('Note not found.');
-        } else {
-          noteData[noteIdx] = {
-            id: noteId,
-            title: req.body.title,
-            text: req.body.text,
-          };
-          setNotes(noteData);
-  
-          note["id"] = noteId;
-  
-          res.status(200).send(note);
-        }
-      } else {
-        res.status(400).send("Invalid request");
-      }
-    } else {
-      res.status(400).send("Invalid request");
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal server error. Please check log.");
-  }
-});
-
+//Delete note data on request
 app.delete("/api/notes/:id", (req, res) => {
   const noteId = parseInt(req.params.id, 10);
+  //Validate and delete note from json
   if (noteId) {
     noteData = noteData.filter((n) => n.id !== noteId);
     setNotes(noteData);
