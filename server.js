d046b0007539bf28143bc586cac440da171fcdc1
kalyani joshi <kalyani121@gmail.com>
2021-09-22 17:58:02 -0400

    updates to routes

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 const { json } = require("body-parser");
-const { getNotes, setNotes } = require("./util/util");
-const api = require('./routes/index.js');
+const path = require('path');
+const notesRouter = require('./routes/notes');
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.static("public"));
 app.use(json());
+
+//API for notes
+app.use('/api/notes', notesRouter);
+
+//get Home page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
--- util/util.js
+++ util/util.js
@@ -2,14 +2,14 @@ const path = require("path");
 const fs = require("fs");
 
 const getNotes = () => {
-  let content = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+  let content = fs.readFileSync(path.join(__dirname, "..", "db/db.json"));
   if (content.toString()) return JSON.parse(content);
   return [];
 };
 
 const setNotes = (notes) => {
   fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
+    path.join(__dirname, "..", "db/db.json"),
     JSON.stringify(notes)
   );
 };
